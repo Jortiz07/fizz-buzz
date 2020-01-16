cf3a05096dc427d99f77f3131995622db31bae87
Jose Ortiz <jose.ortiz@live.com>
2020-01-16 11:04:47 -0700

    Added default roof.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -2,27 +2,29 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
+  private static final int DEFAULT_ROOF = 100;
   public static void main(String[] args) {
-    aResult();
-  }
 
-  public static void aResult() {
-
-
-    for (int i = 1; i <= 100; i++) {
-
-      if (i % 15 == 0) {
-        System.out.println("FizzBuzz");
-      } else if (i % 3 == 0) {
-        System.out.println("Fizz");
-      } else if (i % 5 == 0) {
-          System.out.println("Buzz");
-      } else {
-        System.out.println(i);
-      }
+    int roof = DEFAULT_ROOF;
+    for (int i = 1; i <= roof; i++) {
+      System.out.println(toString(i));
     }
 
   }
+  public static String toString(int num) {
+    String result = "";
+      if (num % 15 == 0) {
+        result = "FizzBuzz";
+      } else if (num % 3 == 0) {
+        result = "Fizz";
+      } else if (num % 5 == 0) {
+        result = "Buzz";
+      } else {
+        result = String.valueOf(num);
+      }
+    return result;
+  }
+
 }
 
 
