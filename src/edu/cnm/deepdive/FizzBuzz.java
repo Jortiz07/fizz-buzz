d7dbcb31d68badd3bcb18f6f640356907e97b836
Jose Ortiz <jose.ortiz@live.com>
2020-01-16 15:04:47 -0700

    Added int root method.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
   private static final int DEFAULT_ROOF = 100;
   public static void main(String[] args) {
 
-    int roof = DEFAULT_ROOF;
+    int roof = (args.length>0) ? Integer.parseInt(args[0]) : DEFAULT_ROOF;
     for (int i = 1; i <= roof; i++) {
       System.out.println(toString(i));
     }
@@ -26,6 +26,3 @@ public class FizzBuzz {
   }
 
 }
-
-
-
